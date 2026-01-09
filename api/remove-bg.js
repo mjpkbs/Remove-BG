5ec29f4ab119e966970d89d978d8978853ebf3f4
mjpkbs <mjpkbs@gmail.com>
2026-01-09 15:51:15 +0900

    fix

--- api/remove-bg.js
+++ api/remove-bg.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
@@ -96,4 +96,4 @@ export default async function handler(req, res) {
     console.error('Unexpected error:', error);
     return res.status(500).json({ error: error.message || 'Internal server error' });
   }
-}
+};
